dadff7998de5674cb2ba9cbf6c6af278a59c5bc4
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-06 00:26:08 +0500

    add employee component styled

--- src/components/AddEmployee.tsx
+++ src/components/AddEmployee.tsx
@@ -1,7 +1,26 @@
 
 const AddEmployee = () => {
   return (
-    <div>AddEmployee</div>
+    <>
+        <form>
+            <div>
+                <label>First Name : </label>
+                <input type="text" placeholder="First Name" className="border" />
+            </div>
+            <div>
+                <label>Last Name : </label>
+                <input type="text" placeholder="Last Name" />
+            </div>
+            <div>
+                <label>Email : </label>
+                <input type="text" placeholder="Email" />
+            </div>
+            <div>
+                <input type="button" value="Back"  className="px-2 bg-slate-400 rounded" />
+                <input type="submit" value="Add Employee" className="px-2 bg-green-500 rounded" />
+            </div>
+        </form>
+    </>
   )
 }
 
