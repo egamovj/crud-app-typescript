1effca3924a862065dc7967568145541ec2a9e1b
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-05 23:26:57 +0500

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <>
+        <article>
+            <header>
+                <h1>Simple CRUD Application</h1>
+            </header>
+        </article>
+    </>
+  )
+}
+
+export default Home
