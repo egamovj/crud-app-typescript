c5ff8057e4f956f2c215b28a18a1277df8fe60fe
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-06 23:28:45 +0500

    homepage style changed

--- src/components/EmployeeModal.tsx
+++ src/components/EmployeeModal.tsx
@@ -13,23 +13,23 @@ const EmployeeModal = (props: Props) => {
       id="myModal"
       className="fixed z-[1] pt-[200px] left-0 top-0 w-full h-full overflow-auto bg-[rgb(0,0,0)] bg-[rgba(0,0,0,0.4)]"
     >
-      <div className="bg-[#fefefe] m-auto p-5 border border-[#888] w-[50%]">
+      <div className="bg-[#fefefe] m-auto p-5 border border-[#888] w-[50%] rounded-md">
         <span
           onClick={closeBtn}
           className="text-[#aaaaaa] float-right text-[28px] font-bold hover:text-black hover:cursor-pointer hover:no-underline"
         >
           &times;
         </span>
-        <h3>Employee Data</h3>
+        <h3 className="font-mono font-bold text-[20px] text-center">Employee Data</h3>
         <div>
           <div>
-            <label>First Name : {data.firstName}</label>
+            <label className="font-mono font-medium text-[18px]">First Name : {data.firstName}</label>
           </div>
           <div>
-            <label>Last Name : {data.lastName}</label>
+            <label className="font-mono font-medium text-[18px]">Last Name : {data.lastName}</label>
           </div>
           <div>
-            <label>Email Add : {data.email}</label>
+            <label className="font-mono font-medium text-[18px]">Email Add : {data.email}</label>
           </div>
         </div>
       </div>
